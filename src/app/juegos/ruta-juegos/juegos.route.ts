ecd7dd568c68e3eda2b8396f45dbb678e45e29a5
Rodrigo Moreno <84353229+moretvo@users.noreply.github.com>
2024-11-09 18:15:45 -0300

    Reset al Master
    Lo que dice el titulo

--- src/app/juegos/ruta-juegos/juegos.route.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { BuscarJuegosComponent } from '../buscar-juegos/buscar-juegos.component';
-
-export default [
-    {path: 'buscarJuegos', component: BuscarJuegosComponent},
-] as Routes;
